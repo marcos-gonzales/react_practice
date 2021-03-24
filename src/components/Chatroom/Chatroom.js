66cc0bb28c44f915ac193dff4c7ecab7853ac5ca
Marcos Gonzales <markymarrk@gmail.com>
2021-03-23 20:01:51 -0600

    setup chatroom fetch

--- src/components/Chatroom/Chatroom.js
+++ src/components/Chatroom/Chatroom.js
@@ -1,11 +1,20 @@
+import React, { useState, useEffect } from 'react';
 import classes from './Chatroom.module.css';
-import React from 'react';
 
-const Chatroom = () => {
+const Chatroom = ({ user }) => {
+  useEffect(() => {
+    if (!user) return;
+    fetch(`http://localhost:4000/getuser/${user.username}/${user.id}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+
   return (
     <div className={classes.ChatroomContainer}>
-      <div>
+      <div className={classes.ChatroomBox}>
         <h1>Hi from chatroom</h1>
+        <input type='hidden' name={user ? user.username : ''} />
+        <input type='hidden' name={user ? user.id : ''} />
       </div>
     </div>
   );
