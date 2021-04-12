6bbb6133f127c8490515f46d5421ef4e3d23dfbb
Marcos Gonzales <markymarrk@gmail.com>
2021-04-11 20:52:23 -0600

    set up styling with materialize +  media queries

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-import classes from './Signup.module.css'
+import classes from './Signup.module.css';
 
 const Signup = ({
   setUsername,
@@ -9,16 +9,22 @@ const Signup = ({
   username,
   password,
 }) => {
+  const labelStyling = [classes.Label, ' grey-text'];
+
   return (
     <div className={classes.SignUpContainer}>
       <div>
-        <h2>Sign up!</h2>
-        <label>
+        <h2 className='teal-text teal-lighten-2'>Sign up!</h2>
+        <label className={labelStyling[0] + labelStyling[1]}>
           Username
           <input
+            className='white-text text-darken-2 input-field'
             type='text'
             placeholder='John Wick'
             name='username'
+            onKeyDownCapture={(e) =>
+              e.keyCode === 13 ? createUsername(e) : null
+            }
             onChange={(e) => setUsername(e.target.value.toLowerCase())}
           ></input>
         </label>
@@ -28,21 +34,27 @@ const Signup = ({
         <label>
           Password
           <input
+            className='white-text text-darken-2 input-field'
             type='password'
             placeholder='gunsanddogs'
             name='password'
             onChange={(e) => setPassword(e.target.value)}
+            onKeyDownCapture={(e) =>
+              e.keyCode === 13 ? createUsername(e) : null
+            }
           ></input>
         </label>
         <button
+          className='btn-small brown darken-1 white-text waves-effect waves-light'
           onClick={createUsername}
           disabled={username.length >= 3 && password.length >= 5 ? false : true}
         >
+          <i className='material-icons right'>send</i>
           Submit
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
